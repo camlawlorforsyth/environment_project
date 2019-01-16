09ae1e44bd7df53cd0fdc5f3ed5a78513996174b
Cam Lawlor-Forsyth <lawlorfc@myumanitoba.ca>
2019-01-15 18:48:59 -0600

    Update environments_part2.py

--- environments_part2.py
+++ environments_part2.py
@@ -47,6 +47,8 @@ warnings.filterwarnings("ignore") # ignore warnings about division by 0 when
 #   need info/help from Yjan - where is this photometry?
 
 # SDSS # ugriz
+# DR12 spectro info from here:
+#   http://data.sdss3.org/sas/dr12/sdss/spectro/redux/specObj-dr12.fits
 # see here for bandpass info:
 #   http://www.sdss3.org/instruments/camera.php#Filters
 #   http://www.sdss3.org/dr8/algorithms/sdssUBVRITransform.php
@@ -66,10 +68,11 @@ warnings.filterwarnings("ignore") # ignore warnings about division by 0 when
 # data access available through online form, here:
 #   https://outerspace.stsci.edu/display/PANSTARRS/How+to+retrieve+and+use+PS1+data
 #   http://archive.stsci.edu/panstarrs/search.php
+#   https://catalogs.mast.stsci.edu/
 
 # 2MASS # uses 2MASS JHK_s
-#xsc_aaa.gz # 2MASS extended source catalog (declination < 0.0°)
-#xsc_baa.gz # 2MASS extended source catalog (declination >= 0.0°)
+#2MASS_xsc_aaa.gz # 2MASS extended source catalog (declination < 0.0°)
+#2MASS_xsc_baa.gz # 2MASS extended source catalog (declination >= 0.0°)
 # see here for bandpass info:
 #    https://old.ipac.caltech.edu/2mass/releases/allsky/doc/sec6_4a.html
 # more info here:
@@ -77,15 +80,31 @@ warnings.filterwarnings("ignore") # ignore warnings about division by 0 when
 #    https://old.ipac.caltech.edu/2mass/releases/allsky/doc/sec1_3.html
 #    https://old.ipac.caltech.edu/2mass/releases/allsky/doc/sec2_3.html
 #    https://irsa.ipac.caltech.edu/2MASS/download/allsky/format_xsc.html
+#    https://old.ipac.caltech.edu/2mass/releases/allsky/doc/sec1_4.html
 # data access available through bulk download, here:
 #    https://irsa.ipac.caltech.edu/2MASS/download/allsky/
+# or through online form, here:
+#    https://irsa.ipac.caltech.edu/applications/Gator/
 
 # WISE # uses AB system
-#WISECat.fits # WISE photometric catalog
 # see here for bandpass info:
 #   http://wise2.ipac.caltech.edu/docs/release/allsky/expsup/sec4_4h.html
 # more info here:
 #    http://wise2.ipac.caltech.edu/docs/release/allwise/
 #    http://wise.ssl.berkeley.edu/astronomers.html
-# data access here:
-#   http://www.gama-survey.org/dr3/data/cat/WISEPhotometry/v02/WISECat.fits
+#    http://wise2.ipac.caltech.edu/docs/release/allwise/expsup/sec1_5.html
+# data access available through online form, here:
+#    https://irsa.ipac.caltech.edu/applications/Gator/
+
+
+
+
+
+lambdar_catalog = fits.open('LambdarCat.fits')
+#info = lambdar_catalog.info()
+header = lambdar_catalog[1].header
+print(header) # to see what is in the actual data table
+data = lambdar_catalog[1].data
+RAs = data.field('RA')*u.deg
+Decs = data.field('DEC')*u.deg
+
